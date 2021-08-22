4fce8767f6df54e292895acba1fe606db443d099
jgomes79 <jgomes79@hotmail.es>
2021-08-22 21:55:56 +0200

    Script to deploy farm

---
+++ migrations/5_deploy_farm.js
@@ -0,0 +1,23 @@
+const HomeCoin = artifacts.require("./HomeCoin");
+const StakeFarm = artifacts.require("./StakeFarm");
+
+async function doDeploy(deployer, network, accounts) {
+
+  let homeCoin = await HomeCoin.at('0xAF585c15daB8C363087c572758AC75E82C467579');
+  console.log(homeCoin.address);
+  
+  await deployer.deploy(StakeFarm, homeCoin.address);
+  let stakeFarm = await StakeFarm.deployed();
+  console.log('StakeFarm deployed:', stakeFarm.address);
+}
+
+
+module.exports = function(deployer, network, accounts) {
+  deployer.then(async () => {
+      await doDeploy(deployer, network, accounts);
+  });
+};
+
+
+
+
